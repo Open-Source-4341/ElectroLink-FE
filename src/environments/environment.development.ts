d19a826a3b9f234b7f3a889bd5d860337ef9e467
WiDDsito <leandro25.cl@gmail.com>
2025-05-13 12:17:50 -0500

    This reverts commit 7c8861189f046c0c4255a298352cd29b2159f59b.

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  // Server Base Path for Fake REST API
-  serverBasePath: 'https://caa9a65d36bebdd4c9be.free.beeceptor.com/api/v1',
-  // Server Base Path for Spring Boot REST API
-  //serverBasePath: 'http://localhost:8090/api/v1',
-  studentsEndpointPath: '/technicians'
- };
