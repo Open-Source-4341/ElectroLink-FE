92bb46fcc967b0de8030aee7548d8bef28071432
WiDDsito <leandro25.cl@gmail.com>
2025-05-13 12:25:20 -0500

    I will restructure the project within the LeandroFiles folder

---
+++ LeandroFiles/.angular/cache/19.2.11/service-management-bc/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,19 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-RK3ZPYBE.js";
+import "./chunk-LXCH3U57.js";
+import "./chunk-SA32CJU4.js";
+import "./chunk-6QNI6B3L.js";
+import "./chunk-PEBH6BBU.js";
+import "./chunk-WPM5VTLQ.js";
+import "./chunk-4S3KYZTJ.js";
+import "./chunk-3OV72XIM.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ LeandroFiles/.angular/cache/19.2.11/service-management-bc/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,52 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-L7PZWICO.js";
+import "./chunk-2BJ7HU6B.js";
+import "./chunk-2O4WY5GE.js";
+import {
+  Dir
+} from "./chunk-RK3ZPYBE.js";
+import "./chunk-7YCVC5XB.js";
+import "./chunk-LXCH3U57.js";
+import "./chunk-SA32CJU4.js";
+import "./chunk-6QNI6B3L.js";
+import "./chunk-PEBH6BBU.js";
+import "./chunk-WPM5VTLQ.js";
+import "./chunk-4S3KYZTJ.js";
+import "./chunk-3OV72XIM.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
